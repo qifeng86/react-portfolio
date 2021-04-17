ef8f37c0d4c0d5c8315c8fb37c14006c0aa4e21f
Qi Feng <qi.feng86@gmail.com>
2021-04-17 17:46:05 -0400

    deploy

---
+++ build/precache-manifest.8fd6d0f1be0eba97358a461da2ee2084.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "698bf6978e9cbf543b6fcf374a2eb797",
+    "url": "/qifeng86/react-portfolio/index.html"
+  },
+  {
+    "revision": "bc50ddd46ef5dbf5dcb3",
+    "url": "/qifeng86/react-portfolio/static/css/main.851f3803.chunk.css"
+  },
+  {
+    "revision": "6e4766372d496e0eee22",
+    "url": "/qifeng86/react-portfolio/static/js/2.2e0509f8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/qifeng86/react-portfolio/static/js/2.2e0509f8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc50ddd46ef5dbf5dcb3",
+    "url": "/qifeng86/react-portfolio/static/js/main.2716640d.chunk.js"
+  },
+  {
+    "revision": "5c68ebe558c00b2397f6",
+    "url": "/qifeng86/react-portfolio/static/js/runtime-main.5231ff56.js"
+  },
+  {
+    "revision": "21b3f547ddd600d746af8d70e111582f",
+    "url": "/qifeng86/react-portfolio/static/media/DSC00484.21b3f547.JPG"
+  },
+  {
+    "revision": "534d08335e9082974481d374d96b80f1",
+    "url": "/qifeng86/react-portfolio/static/media/background.534d0833.jpg"
+  },
+  {
+    "revision": "d7432a0cdfa73775d925192b63eef78b",
+    "url": "/qifeng86/react-portfolio/static/media/bigmood.d7432a0c.jpg"
+  },
+  {
+    "revision": "c424fe7acd65d3bde6472c643e991201",
+    "url": "/qifeng86/react-portfolio/static/media/budgettracker.c424fe7a.jpg"
+  },
+  {
+    "revision": "c24e995de91da268d73fe22c7c4e7f38",
+    "url": "/qifeng86/react-portfolio/static/media/burgerlogger.c24e995d.jpg"
+  },
+  {
+    "revision": "cc745dfff98669f28737556583f2b909",
+    "url": "/qifeng86/react-portfolio/static/media/employeetracker.cc745dff.jpg"
+  },
+  {
+    "revision": "c2606fd3cc61167c6d2622c1b8d3566b",
+    "url": "/qifeng86/react-portfolio/static/media/qi-feng-resume.c2606fd3.pdf"
+  },
+  {
+    "revision": "e71230a53e1c0c59a771b00ef398e8b4",
+    "url": "/qifeng86/react-portfolio/static/media/reactportfolio.e71230a5.jpg"
+  },
+  {
+    "revision": "e113caf7a27dacf22cd36bc874b6fb01",
+    "url": "/qifeng86/react-portfolio/static/media/resume-cv.e113caf7.PNG"
+  },
+  {
+    "revision": "d251631fe22c604aa436fb692ddcf668",
+    "url": "/qifeng86/react-portfolio/static/media/workouttracker.d251631f.jpg"
+  }
+]);
